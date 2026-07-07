f0570adf64a224f3dda0df7194901a22c7a6f755
Nadzhif Fikri <nadzhiff@gmail.com>
2026-07-07 11:23:52 +0800

    config getter setters

--- pkg/factory/config.go
+++ pkg/factory/config.go
@@ -2,14 +2,17 @@ package factory
 
 import (
 	"fmt"
+	"os"
+	"strconv"
 	"sync"
 
 	"github.com/asaskevich/govalidator"
+	"github.com/free5gc/dnf/internal/logger"
 	"github.com/google/uuid"
 )
 
 const (
-	DnfDefaultConfigPath = "./config/dnfcfg.yaml"
+	DnfDefaultConfigPath = "./NFs/dnf/dnfcfg.yaml"
 	DnfSbiDefaultIPv4    = "127.0.0.101"
 	DnfSbiDefaultPort    = 8000
 	DnfSbiDefaultScheme  = "https"
@@ -96,4 +99,129 @@ func appendInvalid(err error) error {
 	}
 
 	return error(errs)
+}
+
+func (c *Config) GetVersion() string {
+	c.RWMutex.RLock()
+	defer c.RWMutex.RUnlock()
+
+	if c.Info.Version != "" {
+		return c.Info.Version
+	}
+	return ""
+}
+
+func (c *Config) SetLogEnable(enable bool) {
+	c.RWMutex.Lock()
+	defer c.RWMutex.Unlock()
+
+	if c.Logger == nil {
+		logger.CfgLog.Warnf("Logger should not be nil")
+		c.Logger = &Logger{
+			Enable: enable,
+			Level:  "info",
+		}
+	} else {
+		c.Logger.Enable = enable
+	}
+}
+
+func (c *Config) SetLogLevel(level string) {
+	c.RWMutex.Lock()
+	defer c.RWMutex.Unlock()
+
+	if c.Logger == nil {
+		logger.CfgLog.Warnf("Logger should not be nil")
+		c.Logger = &Logger{
+			Level: level,
+		}
+	} else {
+		c.Logger.Level = level
+	}
+}
+
+func (c *Config) SetLogReportCaller(reportCaller bool) {
+	c.RWMutex.Lock()
+	defer c.RWMutex.Unlock()
+
+	if c.Logger == nil {
+		logger.CfgLog.Warnf("Logger should not be nil")
+		c.Logger = &Logger{
+			Level:        "info",
+			ReportCaller: reportCaller,
+		}
+	} else {
+		c.Logger.ReportCaller = reportCaller
+	}
+}
+
+func (c *Config) GetLogEnable() bool {
+	c.RWMutex.RLock()
+	defer c.RWMutex.RUnlock()
+	if c.Logger == nil {
+		logger.CfgLog.Warnf("Logger should not be nil")
+		return false
+	}
+	return c.Logger.Enable
+}
+
+func (c *Config) GetLogLevel() string {
+	c.RWMutex.RLock()
+	defer c.RWMutex.RUnlock()
+	if c.Logger == nil {
+		logger.CfgLog.Warnf("Logger should not be nil")
+		return "info"
+	}
+	return c.Logger.Level
+}
+
+func (c *Config) GetLogReportCaller() bool {
+	c.RWMutex.RLock()
+	defer c.RWMutex.RUnlock()
+	if c.Logger == nil {
+		logger.CfgLog.Warnf("Logger should not be nil")
+		return false
+	}
+	return c.Logger.ReportCaller
+}
+
+func (c *Config) GetSbiBindingAddr() string {
+	c.RLock()
+	defer c.RUnlock()
+	return c.GetSbiBindingIP() + ":" + strconv.Itoa(c.GetSbiPort())
+}
+
+func (c *Config) GetSbiBindingIP() string {
+	c.RLock()
+	defer c.RUnlock()
+	bindIP := "0.0.0.0"
+	if c.Configuration == nil || c.Configuration.Sbi == nil {
+		return bindIP
+	}
+	if c.Configuration.Sbi.BindingIPv4 != "" {
+		if bindIP = os.Getenv(c.Configuration.Sbi.BindingIPv4); bindIP != "" {
+			logger.CfgLog.Infof("Parsing ServerIPv4 [%s] from ENV Variable", bindIP)
+		} else {
+			bindIP = c.Configuration.Sbi.BindingIPv4
+		}
+	}
+	return bindIP
+}
+
+func (c *Config) GetSbiPort() int {
+	c.RLock()
+	defer c.RUnlock()
+	if c.Configuration != nil && c.Configuration.Sbi != nil && c.Configuration.Sbi.Port != 0 {
+		return c.Configuration.Sbi.Port
+	}
+	return DnfSbiDefaultPort
+}
+
+func (c *Config) GetSbiScheme() string {
+	c.RLock()
+	defer c.RUnlock()
+	if c.Configuration != nil && c.Configuration.Sbi != nil && c.Configuration.Sbi.Scheme != "" {
+		return c.Configuration.Sbi.Scheme
+	}
+	return DnfSbiDefaultScheme
 }
