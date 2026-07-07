01320247ad69c007cde783dc2c153a8f2e53d398
Nadzhif Fikri <nadzhiff@gmail.com>
2026-07-07 11:04:09 +0800

    service draft

--- pkg/service/init.go
+++ pkg/service/init.go
@@ -1,1 +1,22 @@
 package service
+
+import (
+	"context"
+
+	"github.com/free5gc/dnf/pkg/factory"
+)
+
+type DnfApp struct {
+	cfg *factory.Config
+
+	ctx context.Context
+}
+
+func NewApp(ctx context.Context, cfg *factory.Config) (*DnfApp, error) {
+	dnf := &DnfApp{
+		cfg: cfg,
+		ctx: ctx,
+	}
+
+	return dnf, nil
+}
