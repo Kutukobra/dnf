c4d5f7a39bcc5cdbdf59997fce41147a33e70283
Nadzhif Fikri <nadzhiff@gmail.com>
2026-07-14 14:50:57 +0800

    simplest endpoint

--- internal/sbi/api_dummy.go
+++ internal/sbi/api_dummy.go
@@ -1,5 +1,20 @@
 package sbi
 
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
 func (s *Server) getDummyRoutes() []Route {
-	return []Route{}
+	return []Route{
+		{
+			Name:    "Index",
+			Method:  http.MethodGet,
+			Pattern: "/",
+			APIFunc: func(c *gin.Context) {
+				c.String(http.StatusOK, "Hello DNF!")
+			},
+		},
+	}
 }
