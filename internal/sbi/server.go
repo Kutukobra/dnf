292fba8584263c42ccaa0eb3f1d50baa15c2494f
Nadzhif Fikri <nadzhiff@gmail.com>
2026-07-14 09:01:23 +0800

    sbi

--- internal/sbi/server.go
+++ internal/sbi/server.go
@@ -1,13 +1,18 @@
 package sbi
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
 
+	"github.com/free5gc/dnf/internal/logger"
 	"github.com/free5gc/dnf/internal/sbi/consumer"
 	"github.com/free5gc/dnf/internal/sbi/processor"
 	"github.com/free5gc/dnf/pkg/app"
+	"github.com/free5gc/util/httpwrapper"
+	logger_util "github.com/free5gc/util/logger"
 )
 
 type ServerDnf interface {
@@ -29,5 +34,23 @@ func NewServer(dnf ServerDnf) (*Server, error) {
 		ServerDnf: dnf,
 	}
 
+	s.router = newRouter(s)
+
+	cfg := s.Config()
+	bindAddr := cfg.GetSbiBindingAddr()
+	logger.SBILog.Infof("Binding addr: [%s]", bindAddr)
+	var err error
+	if s.httpServer, err = httpwrapper.NewHttp2Server(bindAddr, "", s.router); err != nil {
+		logger.InitLog.Errorf("Initialize HTTP server failed: %v", err)
+		return nil, err
+	}
+	s.httpServer.ErrorLog = log.New(logger.SBILog.WriterLevel(logrus.ErrorLevel), "HTTP2: ", 0)
+
 	return s, nil
+}
+
+func newRouter(s *Server) *gin.Engine {
+	router := logger_util.NewGinWithLogrus(logger.GinLog)
+
+	return router
 }
